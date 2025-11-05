8805dd4f460f9fe4179b3d9d1f4b5e711de395c2
jawwad9 <jawwadmuhammad514@gmail.com>
2025-11-05 11:36:38 -0800

    function complete

--- 03-/src/App.jsx
+++ 03-/src/App.jsx
@@ -1,8 +1,22 @@
 
 
 function App() {
+
+  console.log("ok");
+  
   return (
     <>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
+    <h1>hellow World</h1>
     <h1>hellow World</h1>
     </>
   )
