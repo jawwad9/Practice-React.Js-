c51f8ed3f3db31115777de513c84c5a4c9842ec7
jawwad9 <jawwadmuhammad514@gmail.com>
2025-11-12 22:57:51 -0800

    complete

--- 04-react-rout-dom/src/screens/Home.jsx
+++ 04-react-rout-dom/src/screens/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+
+    </>
   )
 }
 
