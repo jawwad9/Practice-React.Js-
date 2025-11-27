a5d7ba1a9969cdd3d0ac5ba12048d2ea1f36570d
jawwad9 <jawwadmuhammad514@gmail.com>
2025-11-27 13:13:40 -0800

    react router dom install

--- 05-api-call/src/Screens/About.jsx
+++ 05-api-call/src/Screens/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>Abofdghngjmlyokutyjmnhtdbgvgbhnjmk,li;ou'iplkumjgfbvbhdjnkiuoly,k,mhjfgbvut</div>
+    <div>About</div>
   )
 }
 
--- 05-api-call/src/Screens/Contact.jsx
+++ 05-api-call/src/Screens/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Conta4owthspkld;gbxnjhisgepdfgdfgfdrz;lkbgmnhjpgsfez;lkgbn:OFKgct</div>
+    <div>Contact</div>
   )
 }
 
