3784572981227f6e48868c8ab544ac084489cf5d
jawwad9 <jawwadmuhammad514@gmail.com>
2025-12-01 14:01:58 -0800

    complete

--- 05-api-call/src/Screens/Home.jsx
+++ 05-api-call/src/Screens/Home.jsx
@@ -1,23 +1,8 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
 
 const Home = () => {
 
-  const [product, setProduct] = useState()
-
-useEffect(()=>{
-
-  const data = axios("")
-  .than((res)=>{
-    console.log(res);
-    
-  }).chat((err)=>{
-    console.log(err);
-    
-  })
-},[])
   return (
-    <div>Home ihugfdftyguhijoipouyitrytrsaesrdtfyguiuoip;uohgfjhdgcjhkjkj</div>
+    <div>Home</div>
   )
 }
 
