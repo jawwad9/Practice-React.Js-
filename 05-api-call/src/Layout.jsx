5544f4a738846eb7cf776e19b956753558c139eb
jawwad9 <jawwadmuhammad514@gmail.com>
2025-12-04 13:20:39 -0800

    matriel ui install

--- 05-api-call/src/Layout.jsx
+++ 05-api-call/src/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import { Outlet } from 'react-router-dom'
+import { Card } from '@mui/material'
 
 const Layout = () => {
   return (
     <>
      <Navbar />
      <Outlet />
+     <Card />
 
 
     </>
